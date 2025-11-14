81d4f1bf6a4dac94bcae3d80991f7dd36eb4b77f
Yashaswini2004 <yashaswinic31@gmail.com>
2025-11-14 13:18:00 +0530

    validations

---
+++ js/create.js
@@ -0,0 +1,77 @@
+let f_name=document.getElementById("name");
+let f_email=document.getElementById("email");
+let f_mobile=document.getElementById("mobile");
+let userForm=document.getElementById("userForm");
+let errName=document.getElementById("nameErr");
+let errEmail=document.getElementById("emailErr");
+let errMobile=document.getElementById("mobileErr");
+// submit event listener
+userForm.addEventListener("submit",async(e)=>{
+    e.preventDefault();
+    let user={
+        name:f_name.value,
+        email:f_email.value,
+        mobile:f_mobile.value
+    }
+    if(validate(user))
+    {
+        console.log(`new user=`,user);
+    }
+    else{
+        console.log(`error in the form inputs`);
+    }
+   
+})
+
+// validate 
+function validate(user){
+    let isValid=true;
+    if(!user?.name){
+        errName.innerText="name is required";
+        errName.style.color="red";
+        isValid=false;
+       
+    }
+    else if(!/^[a-zA-A]{2,20}$/.test(user?.name))
+    {
+        errName.innerText="Invalid name format";
+        errName.style.color="red";
+        isValid=false;
+    }
+    {
+
+    }
+     if(!user?.email){
+        errEmail.innerText="email is required";
+        errEmail.style.color="red";
+       
+    }
+    else if(!/\S+@\S+\.\S+/.test(user?.email))
+    {
+         errEmail.innerText="Invalid email format";
+        errEmail.style.color="red";
+        isValid=false;
+
+    }
+     if(!user?.mobile){
+        errMobile.innerText="mobileno is required";
+        errMobile.style.color="red";
+         isValid=false;
+      
+    }
+    else if(!/^(0|\+91)?[6-9]\d{9}$/.test(user?.mobile))
+    {
+        errMobile.innerText="Invalid mobile number format";
+        errMobile.style.color="red";
+        isValid=false;
+
+    }
+    setTimeout(()=>{
+      errName.innerText=" ";
+      errEmail.innerText=" ";
+      errMobile.innerText=" ";
+    },3000)
+    return isValid;
+}
+
+
